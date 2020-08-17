32584a5a3e93316d60915b6ad93151c68a4d4ab3
Macintosh <macintosh@Alexander.local>
2020-08-17 19:56:21 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// alert('welcome to C4EJS94');
+
+// console.log('Welcome to C4EJS94');
+
+// let x = 5;
+// let s = 'Hello world';
+// console.log(x);
+// console.log(s);
+// // x = 6;
+// // s = 'Hi there';
+// console.log(x);
+// console.log(s);
+
+// x = prompt('How old are you?');
+// console.log("I am " + x + " years old");
+
+// let x = 0;
+// while (x<3){
+//     console.log('Today is great')
+//     x++;
+// }
+
+// for (let x = 0; x<3; x++){
+//     console.log('I hate Chinese')
+// }
+
+let age = prompt('what is your age?');
+if (age < 10)
+alert('You are too young');
+else if (age < 18)
+alert('We can go out for a walk');
+alert('You can watch the movie');
